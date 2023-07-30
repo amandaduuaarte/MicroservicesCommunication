42edc4cea276c45cddac5829068e0dee91ce87ef
Amanda Duarte <amandaduuaarte@gmail.com>
2023-07-30 20:39:10 -0300

    feat: Validating the entire flow of get accesstoken [auth-api]

--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -1,6 +1,11 @@
+import bcrypt from "bcrypt";
+import Jwt  from "jsonwebtoken";
+
 import UserRepository from "../repository/userRepository.js";
-import * as httpStatus from "../../../config/constants/httpStatus.js"
 import UserException from "../exceptions/UserException.js";
+import * as httpStatus from "../../../config/constants/httpStatus.js"
+import * as secrets from "../../../config/constants/secrets.js";
+
 
 class UserService {
     async findByEmail(req) {
@@ -36,6 +41,47 @@ class UserService {
     validateUserNotFound(user) {
         if (!user) {
             throw new UserException(httpStatus.BAD_REQUEST, "User is not found.")
+        }
+    }
+
+    async getAccessToken(req) {
+        try {
+            const { email, password } = req.body;
+            this.validateAccessTokenData(req.body);
+            const user = await UserRepository.findByEmail(email);
+            this.validateUserNotFound(user);
+            await this.validatePassword(password, user.password);
+            const authUser = {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+            }
+            const accessToken = Jwt.sign({ authUser }, secrets.API_SECRET, { expiresIn: "1d" });
+            return {
+                status: httpStatus.SUCCESS,
+                accessToken: accessToken
+            }
+
+        } catch (err) {
+            return {
+                status: err.status || httpStatus.INTERNAL_SERVER_ERROR,
+                message: err.message,
+            }
+        }
+     
+
+    }
+
+    validateAccessTokenData(user) {
+        const { email, password } = user;
+        if (!email && !password) { 
+            throw new UserException(httpStatus.UNAUTHORIZE, "Email or password must be informeted.")
+        }
+    }
+
+    async validatePassword(password, hasPassword) {
+        if (!await bcrypt.compare(password, hasPassword)) {
+            throw new UserException(httpStatus.UNAUTHORIZED, "Password is incorrect");
         }
     }
 }
