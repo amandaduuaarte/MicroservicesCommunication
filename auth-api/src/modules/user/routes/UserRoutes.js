7cb90319410775455bba9a2239153f4a87c1d910
Amanda Duarte <amandaduuaarte@gmail.com>
2023-07-30 20:37:52 -0300

    feat: Create new route for get accessToken [auth-api]

--- auth-api/src/modules/user/routes/UserRoutes.js
+++ auth-api/src/modules/user/routes/UserRoutes.js
@@ -4,5 +4,7 @@ import UserController from "../controller/userController.js";
 const routes = new Router();
 
 routes.get('/api/user/email/:email', UserController.findByEmail);
+routes.post('/api/user/auth', UserController.getAccessToken);
+
 
 export default routes;
