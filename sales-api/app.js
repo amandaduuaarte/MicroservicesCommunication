f5c2ef337726ed9bc7f6bddddf598704e7ffcd35
Amanda Duarte <amandaduuaarte@gmail.com>
2023-08-21 21:55:16 -0300

    feat: Creating initial data and Order model

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { connect } from './src/config/db/mongoDbConfig.js';
+import {createInitialData} from "./src/config/db/initialData.js"
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 3031;
 
 connect();
+createInitialData();
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
