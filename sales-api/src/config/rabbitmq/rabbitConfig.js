da51d55e91bd8ad495d343c4624a21510e276671
Amanda Duarte <amandaduuaarte@gmail.com>
2023-08-23 18:35:22 -0300

    feat: Listening to the Sales Queue

--- sales-api/src/config/rabbitmq/rabbitConfig.js
+++ sales-api/src/config/rabbitmq/rabbitConfig.js
@@ -7,29 +7,48 @@ import {
     SALES_CONFIRMATION_QUEUE,
     SALES_CONFIRMATION_ROUTING_KEY
 } from './queue.js';
+import { listenToSalesConfirmationQueue } from '../../modules/sales/rabbitmq/salesConfirmationListener.js';
 
-
-const HALF_SECOND = 500;
+const TWO_SECOND = 2000;
+const HALF_MINUTES = 300000;
 export async function connectRabbitMq() {
-    amqp.connect(RABBIT_MQ_URL, (error, connection) => {
-        if (error) {
-            throw error;
-        }
-        createQueue(
-            connection,
-            PRODUCT_STOCK_UPDATE_QUEUE,
-            PRODUCUT_STOCK_UPDATE_ROUTING_KEY,
-            PRODUCT_TOPIC);
-        createQueue(
-            connection,
-            SALES_CONFIRMATION_QUEUE,
-            SALES_CONFIRMATION_ROUTING_KEY,
-            PRODUCT_TOPIC);
+    const env = process.env.NODE_ENV;
+    if (env === 'container') {
+        console.info('Waiting for RabbitMQ to start.');
+        setInterval(() => {
+            connectRabbitMqAndCreateQueues();
+        }, HALF_MINUTES)
+    } else {
+         connectRabbitMqAndCreateQueues();
+    }
+}
+   async function connectRabbitMqAndCreateQueues() {
+        amqp.connect(RABBIT_MQ_URL, (error, connection) => {
+            if (error) {
+                throw error;
+            }
+            console.info('Starting RabbitMQ.');
+            createQueue(
+                connection,
+                PRODUCT_STOCK_UPDATE_QUEUE,
+                PRODUCUT_STOCK_UPDATE_ROUTING_KEY,
+                PRODUCT_TOPIC);
+            createQueue(
+                connection,
+                SALES_CONFIRMATION_QUEUE,
+                SALES_CONFIRMATION_ROUTING_KEY,
+                PRODUCT_TOPIC);
+            
+                console.info('Queues and Topics were defined.');
+            setTimeout(() => {
+                connection.close();
+            }, TWO_SECOND);
+        });
         setTimeout(() => {
-            connection.close();
-        }, HALF_SECOND);
-    });
-
+            listenToSalesConfirmationQueue();
+        }, TWO_SECOND);
+      
+    }
     function createQueue(connection, queue, routingKey, topic) { 
         connection.createChannel((error, channel) => {
             if (error) {
@@ -40,4 +59,3 @@ export async function connectRabbitMq() {
             channel.bindQueue(queue, topic, routingKey);
         })
     }
-}
